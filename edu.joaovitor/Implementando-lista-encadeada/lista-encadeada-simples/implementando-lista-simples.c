c200ac6f69fbc77f44566de256fbed7eba3c83a9
joao vitor <joaovitor646260@gmail.com>
2025-05-27 22:07:56 -0300

    chore: implementando lista simples

--- edu.joaovitor/Implementando-lista-encadeada/lista-encadeada-simples/implementando-lista-simples.c
+++ edu.joaovitor/Implementando-lista-encadeada/lista-encadeada-simples/implementando-lista-simples.c
@@ -1,5 +1,39 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<stdint.h>
 
-typedef {
+typedef struct node {
+    int data;
 
+    struct node* next;
+} node;
+
+void main (){
+
+    node* head = NULL;
+    node* second = NULL;
+    node* third = NULL;
+
+    head = (node*)malloc(sizeof(node));
+    second = (node*)malloc(sizeof(node));
+    third = (node*)malloc(sizeof(node));
+
+    if(!head || !second || !third){
+        printf("memoria não alocada");
+    }
+    head -> data = 10;
+    head -> next = second;
+
+    second -> data = 22;
+    second -> next = third;
+
+    third -> data = 30;
+    third -> next = NULL;
+
+    node* corrente = head;
+
+    while(corrente != NULL){
+        printf("_%d_", corrente->data);
+        corrente = corrente -> next;
+    }
 }
